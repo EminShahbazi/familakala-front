d548e2e15d27da0ebd26b54f7a9e065477324823
EminShahbazi <emin.shahbazi@outlook.com>
2022-03-05 13:49:19 +0330

    add gulp js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require("gulp");
 const sass = require("gulp-sass")(require("node-sass"));
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const babel = require("gulp-babel");
 const browserSync = require("browser-sync").create();
 
 const { task, src, dest, watch, series } = gulp;
@@ -11,6 +12,11 @@ const path = {
         src: ["resources/scss/**/*.scss"],
         public: ["public/assets/css"],
         build: ["build/assets/css"],
+    },
+    js: {
+        src: ["resources/js/**/*.js"],
+        public: ["public/assets/js"],
+        build: ["build/assets/js"],
     },
     html: "public/**/*.html",
 };
@@ -27,11 +33,22 @@ task("scss", () => {
         .pipe(browserSync.stream());
 });
 
+task("js", () => {
+    return src(path.js.src)
+        .pipe(
+            babel({
+                presets: ["@babel/env"],
+            })
+        )
+        .pipe(dest(path.js.public));
+});
+
 exports.default = function () {
     browserSync.init({
         server: "./public",
     });
 
     watch(path.scss.src, series(["scss"]));
+    watch(path.js.src).on("change", series(["js", browserSync.reload]));
     watch(path.html).on("change", browserSync.reload);
 };
