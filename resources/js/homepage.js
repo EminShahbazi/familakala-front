2a59546d0282b545e86e882512102128fc2362b5
EminShahbazi <emin.shahbazi@outlook.com>
2022-03-10 11:00:21 +0330

    + best-products-slider

--- resources/js/homepage.js
+++ resources/js/homepage.js
@@ -45,4 +45,43 @@ $(document).ready(function () {
             },
         ],
     });
+
+    $("#best-products-slider").slick({
+        infinite: false,
+        arrows: false,
+        slidesToShow: 4.2,
+        slidesToScroll: 1,
+
+        // prevArrow: $("#js-discounts-slider-prev"),
+        // nextArrow: $("#js-discounts-slider-next"),
+        rtl: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3.2,
+                },
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2.2,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2.1,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true,
+                    infinite: true,
+                },
+            },
+        ],
+    });
 });
