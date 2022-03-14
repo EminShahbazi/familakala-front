41c459bb51e7744dd1ef56af10be3f5a3beec378
EminShahbazi <emin.shahbazi@outlook.com>
2022-03-14 17:49:31 +0330

    + Mobile Menu

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,33 @@
 "use strict";
 
+var $body = $(document.body);
+var $bodyMask = $("#js-body-mask");
+
+var mobileMenu = function () {
+  // Cache DOM
+  var $menuToggler = $("#js-mobile-menu-toggler");
+  var $menu = $("#js-mobile-menu");
+  var $togglers = $menu.find(".mobile-nav-item .heading"); // Event Listners
+
+  $menuToggler.on("click", openMenu);
+  $togglers.on("click", openMenuItem);
+  $bodyMask.on("click", closeMenu); // Event Handles
+
+  function openMenu() {
+    $body.addClass("has-overlay");
+    $menu.addClass("open");
+  }
+
+  function closeMenu() {
+    $menu.removeClass("open");
+    $body.removeClass("has-overlay");
+  }
+
+  function openMenuItem(event) {
+    $(event.target).parent(".mobile-nav-item").toggleClass("active");
+  }
+}();
+
 var headerCats = function () {
   // Cache DOM
   var $container = $("#js-header-cats");
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,32 @@
+const $body = $(document.body);
+const $bodyMask = $("#js-body-mask");
+
+const mobileMenu = (function () {
+    // Cache DOM
+    const $menuToggler = $("#js-mobile-menu-toggler");
+    const $menu = $("#js-mobile-menu");
+    const $togglers = $menu.find(".mobile-nav-item .heading");
+
+    // Event Listners
+    $menuToggler.on("click", openMenu);
+    $togglers.on("click", openMenuItem);
+    $bodyMask.on("click", closeMenu);
+
+    // Event Handles
+    function openMenu() {
+        $body.addClass("has-overlay");
+        $menu.addClass("open");
+    }
+
+    function closeMenu() {
+        $menu.removeClass("open");
+        $body.removeClass("has-overlay");
+    }
+
+    function openMenuItem(event) {
+        $(event.target).parent(".mobile-nav-item").toggleClass("active");
+    }
+})();
 const headerCats = (function () {
     // Cache DOM
     const $container = $("#js-header-cats");
