96836ab08c67436392815480b6150592db4926c5
EminShahbazi <emin.shahbazi@outlook.com>
2022-04-10 23:27:47 +0430

    Products Page Ajax Filter

--- public/assets/js/shop.js
+++ public/assets/js/shop.js
@@ -1,16 +1,27 @@
 "use strict";
 
 var filterProducts = function () {
+  var page = 1;
   var minPrice = null;
-  var maxPrice = null; // Cache DOM
+  var maxPrice = null;
+  var brands = [];
+  var colors = []; // Cache DOM
 
-  var priceSlider = document.getElementById("price-slider");
+  var $productsListWrapper = $("#products-list-wrapper");
+  var $paginationLinks = $productsListWrapper.find(".js-pagination-link");
+  var priceSlider = $("#price-slider")[0];
   var $priceMin = $("#price-filter-min");
   var $priceMax = $("#price-filter-max");
   var $priceButton = $("#price-filter-submit");
+  var $brandsInput = $("input.brand");
+  var $colorsInput = $("input.color");
   var $mobileFiltersToggler = $("#products-filters-toggler");
   var $mobileFilters = $("#products-filters"); // Events
 
+  $paginationLinks.on("click", changePage);
+  $priceButton.on("click", alterQueryString);
+  $brandsInput.on("change", handleBrandChange);
+  $colorsInput.on("change", handleColorChange);
   $mobileFiltersToggler.on("click", openFilters);
   $bodyMask.on("click", closeFilters); // Init
 
@@ -33,16 +44,53 @@ var filterProducts = function () {
       priceSlider.noUiSlider.on("update", function (values, handle) {
         minPrice = Math.round(values[0]);
         maxPrice = Math.round(values[1]);
-        $priceMin.html(Math.round(values[0]).toLocaleString("fa-IR", {
-          style: "decimal"
-        }));
-        $priceMax.html(Math.round(values[1]).toLocaleString("fa-IR", {
-          style: "decimal"
-        }));
+        $priceMin.html(Math.round(values[0]));
+        $priceMax.html(Math.round(values[1]));
       });
     }
-  } // Events
+  } // Event Handlers
 
+
+  function changePage(e) {
+    e.preventDefault();
+    var $this = $(e.currentTarget);
+    page = $this.data("page");
+    alterQueryString(false);
+  }
+
+  function handleBrandChange(e) {
+    var id = $(e.target).val(); //e.target.dataset.id;
+
+    var index = brands.findIndex(function (b) {
+      return b == id;
+    });
+
+    if (index === -1) {
+      brands.push(id);
+    } else {
+      brands.splice(index, 1);
+    }
+
+    page = 1;
+    alterQueryString();
+  }
+
+  function handleColorChange(e) {
+    var id = $(e.target).val(); //e.target.dataset.id;
+
+    var index = brands.findIndex(function (b) {
+      return b == id;
+    });
+
+    if (index === -1) {
+      colors.push(id);
+    } else {
+      colors.splice(index, 1);
+    }
+
+    page = 1;
+    alterQueryString();
+  }
 
   function openFilters() {
     $body.addClass("has-overlay");
@@ -52,6 +100,64 @@ var filterProducts = function () {
   function closeFilters() {
     $mobileFilters.removeClass("open");
     $body.removeClass("has-overlay");
+  } // Helper Functions
+
+
+  function alterQueryString() {
+    var resetPage = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : true;
+    var query = [];
+
+    if (resetPage) {
+      query.push("page=1");
+    } else if (page) {
+      query.push("page=" + page);
+    }
+
+    if (minPrice !== $priceMin.data("range") || maxPrice !== $priceMax.data("range")) {
+      if (minPrice) {
+        query.push("min_price=" + minPrice);
+      }
+
+      if (maxPrice) {
+        query.push("max_price=" + maxPrice);
+      }
+    }
+
+    if (brands.length) {
+      brands.forEach(function (b) {
+        query.push("brands[]=" + b);
+      });
+    }
+
+    if (colors.length) {
+      colors.forEach(function (b) {
+        query.push("colors[]=" + b);
+      });
+    }
+
+    var queryString = "?" + query.join("&");
+    history.pushState({}, "", queryString);
+    getProducts(queryString);
+  }
+
+  function getProducts(querystring) {
+    pageLoading.show();
+    $.ajax({
+      url: "/API_PATH" + location.pathname + querystring,
+      success: function success(data) {
+        // set new HTML data
+        $productsListWrapper.html(data.products); // attach event listner for the new pagination links
+
+        $(".js-pagination-link").on("click", changePage); // scroll to products list top
+
+        $([document.documentElement, document.body]).animate({
+          scrollTop: $productsListWrapper.offset().top
+        }, 500);
+      },
+      complete: function complete() {
+        pageLoading.hide();
+      }
+    });
   }
 }();
 
--- resources/js/shop.js
+++ resources/js/shop.js
@@ -1,16 +1,31 @@
 const filterProducts = (function () {
+    let page = 1;
     let minPrice = null;
     let maxPrice = null;
+    let brands = [];
+    let colors = [];
 
     // Cache DOM
-    const priceSlider = document.getElementById("price-slider");
+    const $productsListWrapper = $("#products-list-wrapper");
+    const $paginationLinks = $productsListWrapper.find(".js-pagination-link");
+
+    const priceSlider = $("#price-slider")[0];
     const $priceMin = $("#price-filter-min");
     const $priceMax = $("#price-filter-max");
     const $priceButton = $("#price-filter-submit");
+
+    const $brandsInput = $("input.brand");
+    const $colorsInput = $("input.color");
+
     const $mobileFiltersToggler = $("#products-filters-toggler");
     const $mobileFilters = $("#products-filters");
 
     // Events
+    $paginationLinks.on("click", changePage);
+    $priceButton.on("click", alterQueryString);
+    $brandsInput.on("change", handleBrandChange);
+    $colorsInput.on("change", handleColorChange);
+
     $mobileFiltersToggler.on("click", openFilters);
     $bodyMask.on("click", closeFilters);
 
@@ -35,21 +50,46 @@ const filterProducts = (function () {
             priceSlider.noUiSlider.on("update", function (values, handle) {
                 minPrice = Math.round(values[0]);
                 maxPrice = Math.round(values[1]);
-                $priceMin.html(
-                    Math.round(values[0]).toLocaleString("fa-IR", {
-                        style: "decimal",
-                    })
-                );
-                $priceMax.html(
-                    Math.round(values[1]).toLocaleString("fa-IR", {
-                        style: "decimal",
-                    })
-                );
+                $priceMin.html(Math.round(values[0]));
+                $priceMax.html(Math.round(values[1]));
             });
         }
     }
 
-    // Events
+    // Event Handlers
+    function changePage(e) {
+        e.preventDefault();
+        const $this = $(e.currentTarget);
+        page = $this.data("page");
+        alterQueryString(false);
+    }
+
+    function handleBrandChange(e) {
+        const id = $(e.target).val(); //e.target.dataset.id;
+        const index = brands.findIndex((b) => b == id);
+        if (index === -1) {
+            brands.push(id);
+        } else {
+            brands.splice(index, 1);
+        }
+
+        page = 1;
+        alterQueryString();
+    }
+
+    function handleColorChange(e) {
+        const id = $(e.target).val(); //e.target.dataset.id;
+        const index = brands.findIndex((b) => b == id);
+        if (index === -1) {
+            colors.push(id);
+        } else {
+            colors.splice(index, 1);
+        }
+
+        page = 1;
+        alterQueryString();
+    }
+
     function openFilters() {
         $body.addClass("has-overlay");
         $mobileFilters.addClass("open");
@@ -58,6 +98,70 @@ const filterProducts = (function () {
     function closeFilters() {
         $mobileFilters.removeClass("open");
         $body.removeClass("has-overlay");
+    }
+
+    // Helper Functions
+    function alterQueryString(resetPage = true) {
+        let query = [];
+
+        if (resetPage) {
+            query.push("page=1");
+        } else if (page) {
+            query.push("page=" + page);
+        }
+
+        if (
+            minPrice !== $priceMin.data("range") ||
+            maxPrice !== $priceMax.data("range")
+        ) {
+            if (minPrice) {
+                query.push("min_price=" + minPrice);
+            }
+
+            if (maxPrice) {
+                query.push("max_price=" + maxPrice);
+            }
+        }
+
+        if (brands.length) {
+            brands.forEach((b) => {
+                query.push("brands[]=" + b);
+            });
+        }
+
+        if (colors.length) {
+            colors.forEach((b) => {
+                query.push("colors[]=" + b);
+            });
+        }
+
+        const queryString = "?" + query.join("&");
+        history.pushState({}, "", queryString);
+        getProducts(queryString);
+    }
+
+    function getProducts(querystring) {
+        pageLoading.show();
+
+        $.ajax({
+            url: "/API_PATH" + location.pathname + querystring,
+            success: function (data) {
+                // set new HTML data
+                $productsListWrapper.html(data.products);
+                // attach event listner for the new pagination links
+                $(".js-pagination-link").on("click", changePage);
+                // scroll to products list top
+                $([document.documentElement, document.body]).animate(
+                    {
+                        scrollTop: $productsListWrapper.offset().top,
+                    },
+                    500
+                );
+            },
+            complete: function () {
+                pageLoading.hide();
+            },
+        });
     }
 })();
 
