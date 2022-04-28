8c6dbdb2229f8207c48fa3edfeec1fa11ac1aa3e
EminShahbazi <emin.shahbazi@outlook.com>
2022-04-28 23:17:25 +0430

    gulp: add build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,11 @@ const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const babel = require("gulp-babel");
 const browserSync = require("browser-sync").create();
+const cssmin = require("gulp-clean-css");
+const jsmin = require("gulp-uglify");
+const gulpIf = require("gulp-if");
+const rev = require("gulp-rev");
+const revReplace = require("gulp-rev-replace");
 
 const { task, src, dest, watch, series } = gulp;
 
@@ -43,6 +48,41 @@ task("js", () => {
         .pipe(dest(path.js.public));
 });
 
+// Build Tasks
+task("moveImages", () => {
+    return src(["public/assets/images/**.*", "public/uploads/**.*"], {
+        base: "public",
+    }).pipe(dest("build"));
+});
+task("moveFonts", () => {
+    return src("public/assets/fonts/**.*").pipe(dest("build/assets/fonts"));
+});
+
+task("rev", () => {
+    return src(["public/assets/css/*.css", "public/assets/js/*.js"], {
+        base: "public/assets/",
+    })
+        .pipe(gulpIf("*.css", cssmin()))
+        .pipe(gulpIf("*.js", jsmin()))
+        .pipe(rev())
+        .pipe(dest("build/assets"))
+        .pipe(rev.manifest())
+        .pipe(dest("build/assets"));
+});
+
+task("revReplace", () => {
+    const manifest = src("build/assets/rev-manifest.json");
+
+    return src("public/**/*.html")
+        .pipe(
+            revReplace({ manifest: manifest, replaceInExtensions: [".html"] })
+        )
+        .pipe(dest("build"));
+});
+
+task("build", series(["moveImages", "moveFonts", "rev", "revReplace"]));
+
+// Default Task
 exports.default = function () {
     browserSync.init({
         server: "./public",
